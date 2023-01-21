874798643c7c54d9fcf7715cda7f1cfc0ea92864
Andrey Monchencko <monchenckoandrey@gmail.com>
2023-01-22 00:31:33 +0600

    debugg

--- JS-scripts/expand-image.js
+++ JS-scripts/expand-image.js
@@ -1,15 +1,12 @@
 const openImage = document.getElementById('open__image');
-const closeImage = document.getElementById('close__image');
 const useImage = document.getElementById('usable__image');
 const buttonRow = document.getElementById('button__row');
-let arrows = document.querySelectorAll('.inner__img');
+const arrows = document.querySelectorAll('.inner__img');
 
 openImage.addEventListener('click', function(){
-
-    arrows[0].classList.toggle('rotate');
-    arrows[1].classList.toggle('rotate');
-    arrows[2].classList.toggle('rotate');
-    arrows[3].classList.toggle('rotate');
+    arrows.forEach((item) => {
+        item.classList.toggle('rotate');
+    })
     useImage.classList.toggle('expand');
     buttonRow.classList.toggle('scale');
 });
